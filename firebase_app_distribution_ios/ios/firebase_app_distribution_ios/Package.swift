06cffb07c8802029baff79cfe084d8327cb1fd0b
Thomas Pucci <thomas.lb.pucci@gmail.com>
2026-03-18 17:23:03 +0100

    chore: update minimum iOS platform version to 15.0

--- firebase_app_distribution_ios/ios/firebase_app_distribution_ios/Package.swift
+++ firebase_app_distribution_ios/ios/firebase_app_distribution_ios/Package.swift
@@ -89,7 +89,7 @@ guard let shared_spm_version = Version("\(firebase_core_version_string)\(shared_
 let package = Package(
   name: "firebase_app_distribution_ios",
   platforms: [
-    .iOS("13.0"),
+    .iOS("15.0"),
   ],
   products: [
     .library(name: "firebase-app-distribution-ios", targets: ["firebase_app_distribution_ios"]),
